85d762319b6d934e9012a2390ddc9fc440ad9c11
NikhilAndola <nikhilandola123@gmail.com>
2024-09-07 13:21:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+function App(){
+  return (
+        <View style={{marginTop: 40}}>
+          <Text>
+          Hello world
+          </Text>
+        </View>
+  );
+}
+
+export default App;
